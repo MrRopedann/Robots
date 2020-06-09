6e4ce9c0467053e8afc439f9623fc1bf0445ace7
mrRopedann <mrRopedann@github.com>
2020-06-09 20:23:09 +0500

    Начало работы

--- robots/src/log/Logger.java
+++ robots/src/log/Logger.java
@@ -1,28 +1,20 @@
 package log;
 
-public final class Logger
-{
-    private static final LogWindowSource defaultLogSource;
+public final class Logger {
+    private static final LogWindowSource DEFAULT_LOG_SOURCE;
+
     static {
-        defaultLogSource = new LogWindowSource(100);
-    }
-    
-    private Logger()
-    {
+        DEFAULT_LOG_SOURCE = new LogWindowSource(100);
     }
 
-    public static void debug(String strMessage)
-    {
-        defaultLogSource.append(LogLevel.Debug, strMessage);
+    private Logger(){}
+    public static void debug(String strMessage) {
+        DEFAULT_LOG_SOURCE.append(LogLevel.Debug, strMessage);
     }
-    
-    public static void error(String strMessage)
-    {
-        defaultLogSource.append(LogLevel.Error, strMessage);
+    public static void error(String strMessage) {
+        DEFAULT_LOG_SOURCE.append(LogLevel.Error, strMessage);
     }
-
-    public static LogWindowSource getDefaultLogSource()
-    {
-        return defaultLogSource;
+    public static LogWindowSource getDefaultLogSource() {
+        return DEFAULT_LOG_SOURCE;
     }
 }
